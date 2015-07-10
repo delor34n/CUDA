9ad670681376091201ec88fd77f6a9d3afc57535
Sebastián Lastra Mena <sebastian.lastra@usach.cl>
2015-07-10 17:27:56 -0300

    Add hello world cu.

--- [7] hello world/hello.cu
+++ [7] hello world/hello.cu
@@ -8,3 +8,21 @@ typedef struct {
 int main(){
 	srand(unsigned(time(NULL)));
 }
+
+void init_population(hello_world *population, ga_vector &buffer ) {
+    int tsize = GA_TARGET.size();
+
+    for (int i=0; i<GA_POPSIZE; i++) {
+        ga_struct citizen;
+        
+        citizen.fitness = 0;
+        citizen.str.erase();
+                                                                                                                                                               
+        for (int j=0; j<tsize; j++)
+            citizen.str += (rand() % 90) + 32; 
+
+        population.push_back(citizen);
+    }   
+
+    buffer.resize(GA_POPSIZE);
+}
